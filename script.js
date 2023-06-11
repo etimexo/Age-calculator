c5d23b4f468abaf78e67e83cac42e74b9fa21aa0
Elijah Obisesan <elijahobisesan01@gmail.com>
2023-06-11 20:29:45 +0100

    Update code

--- script.js
+++ script.js
@@ -1,34 +1,17 @@
-// // Prompt the user to enter their birthdate
-// const birthdate = prompt('Enter your birthdate (YYYY-MM-DD):');
-
-// // Create a Date object for the current date
-// const currentDate = new Date();
-
-// // Extract the year, month, and day from the current date
-// const currentYear = currentDate.getFullYear();
-// const currentMonth = currentDate.getMonth() + 1; // Months are zero-based
-// const currentDay = currentDate.getDate();
-
-// // Extract the year, month, and day from the birthdate
-// const birthYear = parseInt(birthdate.substring(0, 4));
-// const birthMonth = parseInt(birthdate.substring(5, 7));
-// const birthDay = parseInt(birthdate.substring(8, 10));
-
-// // Calculate the age
-// let age = currentYear - birthYear;
-
-// // Adjust the age if the current month and day are before the birth month and day
-// if (currentMonth < birthMonth || (currentMonth === birthMonth && currentDay < birthDay)) {
-//   age--;
-// }
-
-// // Display the current year, month, and day
-// console.log('Current Year:', currentYear);
-// console.log('Current Month:', currentMonth);
-// console.log('Current Day:', currentDay);
-
-// // Display the birthdate entered by the user
-// console.log('Birthdate:', birthdate);
-
-// // Display the calculated age
-// console.log('Age:', age);
+let inputEl1 = document.getElementById("input-el1")
+let inputEl2 = document.getElementById("input-el2")
+let inputEl3 = document.getElementById("input-el3")
+let button = document.getElementById("but")
+button.addEventListener("click", function () {
+    let day = inputEl1.value
+    let month = inputEl2.value
+    let year = inputEl3.value
+    let now = new Date()
+    let inputDate = new Date(year, month - 1, day)
+    yearsDifference = now.getFullYear() - inputDate.getFullYear()
+    console.log(yearsDifference)
+    monthsDifference = now.getMonth() - inputDate.getMonth()
+    console.log(monthsDifference)
+    daysDifference = now.getDate()
+    console.log(daysDifference)
+})
