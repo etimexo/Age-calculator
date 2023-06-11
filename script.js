bc07ece762fbf32bb2f39ce4ba099c6109bedf61
Elijah Obisesan <elijahobisesan01@gmail.com>
2023-06-11 22:44:08 +0100

    Edit files

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ let inputEl1 = document.getElementById("input-el1")
 let inputEl2 = document.getElementById("input-el2")
 let inputEl3 = document.getElementById("input-el3")
 let button = document.getElementById("but")
+let spanYear = document.getElementById("span-year")
+let spanMonth = document.getElementById("span-month")
+let spanDay = document.getElementById("span-day")
 button.addEventListener("click", function () {
     let day = inputEl1.value
     let month = inputEl2.value
