23b24fd51bdd22857cae6343c0137bde97e10154
Elijah Obisesan <elijahobisesan01@gmail.com>
2023-06-12 10:54:47 +0100

    Update index.html and script.js files

--- script.js
+++ script.js
@@ -1,39 +1,70 @@
-let inputEl1 = document.getElementById("input-el1")
-let inputEl2 = document.getElementById("input-el2")
-let inputEl3 = document.getElementById("input-el3")
+let inputEl2 = document.querySelector(".input-el2")
+let inputEl3 = document.querySelector(".input-el3")
+let inputEl1 = document.querySelector(".input-el1")
 let button = document.getElementById("but")
 let spanYear = document.getElementById("span-year")
 let spanMonth = document.getElementById("span-month")
 let spanDay = document.getElementById("span-day")
-button.addEventListener("click", function () {
+let yearsDifference
+let monthsDifference
+let daysDifference
+let resultYears
+let resultMonths
+let resultDays
+let intervalId
+function defaultValues() {
+    spanDay.innerHTML = 0 
+    spanMonth.innerHTML = 0
+    spanYear.innerHTML = 0
+}
+defaultValues()
+function incrementCounters() {
+    // Update years
+    if (yearsDifference < resultYears) {
+      yearsDifference++
+      spanYear.innerHTML = yearsDifference + " "
+    }
+  
+    // Update months
+    if (monthsDifference < resultMonths) {
+      monthsDifference++
+      spanMonth.innerHTML = monthsDifference + " "
+    }
+  
+    // Update days
+    if (daysDifference < resultDays) {
+      daysDifference++
+      spanDay.innerHTML = daysDifference + " "
+    }
+  
+    // Stop the interval when all counters reach their respective results
+    if (yearsDifference === resultYears && monthsDifference === resultMonths && daysDifference === resultDays) {
+      clearInterval(intervalId)
+    }
+  }
+  
+  button.addEventListener("click", function () {
     let day = inputEl1.value
     let month = inputEl2.value
     let year = inputEl3.value
     let now = new Date()
     let inputDate = new Date(year, month - 1, day)
-    function years() {
-        let yearsDifference = now.getFullYear() - inputDate.getFullYear()
-        spanYear.innerHTML = yearsDifference + " "
+    yearsDifference = 0
+    monthsDifference = 0
+    daysDifference = 0
+    resultYears = now.getFullYear() - inputDate.getFullYear()
+    resultMonths = now.getMonth() - inputDate.getMonth()
+    resultDays = now.getDate()
+  
+    if (now.getMonth() < inputDate.getMonth() || (now.getMonth() === inputDate.getMonth() && now.getDate() < inputDate.getDate())) {
+      resultYears--
+      resultMonths += 12
     }
-    function months() {
-        let yearsDifference = now.getFullYear() - inputDate.getFullYear()
-        let monthsDifference = now.getMonth() - inputDate.getMonth()
-        if (yearsDifference === 0 && monthsDifference < 0) {
-            monthsDifference = 12 - Math.abs(monthsDifference)
-        }
-        if (yearsDifference > 0 && monthsDifference < 0) {
-            monthsDifference = 12 - Math.abs(monthsDifference)
-        }
-        if (yearsDifference > 0 && monthsDifference < 0) {
-            monthsDifference = 12 * (yearsDifference - 1) + (12 - Math.abs(monthsDifference))
-        }
-        spanMonth.innerHTML = monthsDifference + " "
+  
+    if (resultYears === 0 && resultMonths > 0 && now.getDate() < inputDate.getDate()) {
+      resultMonths--
     }
-    function days() {
-        let daysDifference = now.getDate()
-        spanDay.innerHTML =daysDifference + " "
-    }
-    years()
-    months()
-    days()
-})
+    clearInterval(intervalId)
+    intervalId = setInterval(incrementCounters, 50)
+
+  })
